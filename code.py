ac3cc6977e8c433ce39acf01dd2a4f5ed7d6a21f
Owen Trueblood <jmptable@mit.edu>
2014-11-14 00:01:24 +0000

    Start vlla-shader on webserver start and kill it when webserver dies.

--- code.py
+++ code.py
@@ -1,13 +1,44 @@
+#!/usr/bin/env python
 import web
+import os, sys, signal
+from subprocess import Popen, call
 
 urls = (
           '/', 'index'
           )
 app = web.application(urls, globals())
 
+shaderPath = "./static/shaders/"
+fragShader = "/tmp/frag.glsl"
+
 class index:
     def GET(self):
-        raise web.seeother('/static/html/index.html')
+        render = web.template.render('templates')
+        return render.index(get_shaders())
+        #raise web.seeother('/static/html/index.html')
+
+def signal_handler(signal, frame):
+    try:
+        shader.terminate()
+        print "killed vlla-shader"
+    except Exception as e:
+        print e
+
+    sys.exit(0)
+
+def get_shaders():
+    shaderFiles = [ f for f in os.listdir(shaderPath) if os.path.isfile(os.path.join(shaderPath, f)) ]
+    return shaderFiles
 
 if __name__ == "__main__":
+    global shader
+
+    signal.signal(signal.SIGINT, signal_handler)
+    
+    defaultShader = shaderPath + get_shaders()[0]
+    call(["cp", defaultShader, fragShader])
+    print "Starting " + defaultShader
+
+    #shader = Popen(["vlla-shader", fragShader])
+
     app.run()
